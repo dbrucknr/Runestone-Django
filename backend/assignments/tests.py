0084c30a0a2ff288bef8978a3e8bb2973db4f2e5
Derek Bruckner <dbrucknr@umich.edu>
2020-06-16 09:52:22 -0400

    added basic tests to the assignments and books apps.

--- backend/assignments/tests.py
+++ backend/assignments/tests.py
@@ -1,3 +1,14 @@
 from django.test import TestCase
 
-# Create your tests here.
+from .models import Assignment
+
+class AssignmentModelTest(TestCase):
+
+    @classmethod
+    def setUpTestData(cls):
+        Assignment.objects.create(title='test assignment')
+
+    def test_title_content(self):
+        assignment = Assignment.objects.get(id=1)
+        expected_object_name = f'{assignment.title}'
+        self.assertEquals(expected_object_name, 'test assignment')
--- backend/books/tests.py
+++ backend/books/tests.py
@@ -1,3 +1,14 @@
 from django.test import TestCase
 
-# Create your tests here.
+from .models import Book
+
+class BookModelTest(TestCase):
+
+    @classmethod
+    def setUpTestData(cls):
+        Book.objects.create(title='test book')
+
+    def test_title_content(self):
+        book = Book.objects.get(id=1)
+        expected_object_name = f'{book.title}'
+        self.assertEquals(expected_object_name, 'test book')
