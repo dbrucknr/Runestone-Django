afbc3213b6034bcab71dd006e1b1b4e985c0eaa9
Derek Bruckner <dbrucknr@umich.edu>
2020-06-16 13:37:17 -0400

    create basic test for new courses app.

--- backend/courses/tests.py
+++ backend/courses/tests.py
@@ -1,3 +1,14 @@
 from django.test import TestCase
 
-# Create your tests here.
+from .models import Course
+
+class CourseModelTest(TestCase):
+
+    @classmethod
+    def setUpTestData(cls):
+        Course.objects.create(title='test course')
+
+    def test_title_content(self):
+        course = Course.objects.get(id=1)
+        expected_object_name = f'{course.title}'
+        self.assertEquals(expected_object_name, 'test course')
